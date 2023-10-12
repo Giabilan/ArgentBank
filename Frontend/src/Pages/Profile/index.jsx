066ce5950ae94e6c214706ca6b84afe3de261a78
Rajendran Giabilan <giabilan.r@gmail.com>
2023-10-12 15:53:09 +0200

    added onSubmit form

--- Frontend/src/Pages/Profile/index.jsx
+++ Frontend/src/Pages/Profile/index.jsx
@@ -15,13 +15,15 @@ const Profile = () => {
     setUserCopy({ username: user.userName });
   }, [user]);
 
-  const handleEdit = async () => {
+  const handleEdit = async (e) => {
+    e.preventDefault();
     const response = await editUserName(user.token, userCopy);
     if (response.status === 200) {
       dispatch({
         type: "user/updateUser",
         payload: { userDatas: response.body },
       });
+      setIsEditing(false);
     }
   };
 
@@ -30,49 +32,53 @@ const Profile = () => {
       {isEditing ? (
         <>
           <h1 className="profileTitle">Edit user info</h1>
-          <div className="inputLabelContainer">
-            <label htmlFor="username">Username :</label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              value={userCopy.username}
-              onChange={(event) =>
-                setUserCopy({ username: event.target.value })
-              }
-            />
-          </div>
-          <div className="inputLabelContainer">
-            <label htmlFor="firstname">Firstname :</label>
-            <input
-              type="text"
-              name="firstname"
-              id="firstname"
-              value={user.firstName}
-              disabled
-            />
-          </div>
-          <div className="inputLabelContainer">
-            <label htmlFor="lastname">Lastname :</label>
-            <input
-              type="text"
-              name="lastname"
-              id="lastname"
-              value={user.lastName}
-              disabled
-            />
-          </div>
-          <div className="editButtonContainer">
-            <Button content="Save" onClick={handleEdit} />
-            <Button content="Cancel" onClick={() => setIsEditing(false)} />
-          </div>
+          <form className="formContainer" onSubmit={handleEdit}>
+            <div className="LabelInputContainer">
+              <label htmlFor="username">User name :</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={userCopy.username}
+                onChange={(event) =>
+                  setUserCopy({ username: event.target.value })
+                }
+              />
+            </div>
+            <div className="LabelInputContainer">
+              <label htmlFor="firstname">First name :</label>
+              <input
+                type="text"
+                name="firstname"
+                id="firstname"
+                value={user.firstName}
+                disabled
+              />
+            </div>
+            <div className="LabelInputContainer">
+              <label htmlFor="lastname">Last name :</label>
+              <input
+                type="text"
+                name="lastname"
+                id="lastname"
+                value={user.lastName}
+                disabled
+              />
+            </div>
+            <div className="editButtonContainer">
+              <Button type="submit" content="Save" />
+              <Button content="Cancel" onClick={() => setIsEditing(false)} />
+            </div>
+          </form>
         </>
       ) : (
         <>
           <h1 className="profileTitle">
             Welcome back <br></br> {user.firstName} {user.lastName} !
           </h1>
-          <Button content="Edit Name" onClick={() => setIsEditing(true)} />
+          <div className="editNameButton">
+            <Button content="Edit Name" onClick={() => setIsEditing(true)} />
+          </div>
         </>
       )}
       <AccountCard />
--- Frontend/src/components/Button/index.jsx
+++ Frontend/src/components/Button/index.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-const Button = ({ content, onClick }) => (
-  <button onClick={onClick}>{content}</button>
+const Button = ({ content, onClick, type }) => (
+  <button type={type} onClick={onClick}>{content}</button>
 );
 
 Button.propTypes = {
   content: PropTypes.string.isRequired,
   onClick: PropTypes.func,
+  type: PropTypes.string,
 };
 
 export default Button;
